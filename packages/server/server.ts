cbbf654e7db0b72f777d4188ad9faecfa959430d
Scharif47 <scharif47@outlook.de>
2024-01-02 22:20:47 +0100

    backend session storage

--- packages/server/server.ts
+++ packages/server/server.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
-import session from 'express-session';
+import session from "express-session";
+import MongoDBStore from "connect-mongodb-session";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 mongoose
   .connect("mongodb://localhost:27017/shcarDB")
@@ -11,11 +15,32 @@ mongoose
     console.error("error connecting to mongodb", err);
   });
 
+const MongoDBStoreSession = MongoDBStore(session);
+const store = new MongoDBStoreSession({
+  uri: "mongodb://localhost:27017/shcarDB",
+  collection: "mySessions",
+});
+
 const app = express();
 const port = process.env.PORT || 3000;
 
+const sessionSecret = process.env.SESSION_SECRET;
+if (!sessionSecret) {
+  throw new Error("SESSION_SECRET is not set");
+}
+
+app.use(
+  session({
+    secret: sessionSecret,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 }, // 1 day
+    store: store,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
 app.get("/", (req, res) => {
-  res.send("Hello Friend!");
+  res.send("Hello Friend!" + JSON.stringify(req.session));
 });
 
 app.listen(port, () => {
