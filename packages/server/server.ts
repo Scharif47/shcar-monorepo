a2d7b485e2e717ba4a96c58c7c4aa927df85faeb
Scharif47 <scharif47@outlook.de>
2024-01-07 09:32:47 +0100

    mongodb connected

--- packages/server/server.ts
+++ packages/server/server.ts
@@ -1,14 +1,20 @@
 import express from "express";
 import mongoose from "mongoose";
 import MongoDBStore from "connect-mongodb-session";
+import morgan from "morgan";
 import dotenv from "dotenv";
 
-const session = require('express-session');
+const session = require("express-session");
 
 dotenv.config();
 
+const mongoUri = process.env.MONGODB_URI;
+if (!mongoUri) {
+  throw new Error("MONGODB_URI is not set");
+}
+
 mongoose
-  .connect("mongodb://localhost:27017/shcarDB")
+  .connect(mongoUri)
   .then(() => {
     console.log("connected to mongodb");
   })
@@ -24,11 +30,13 @@ const store = new MongoDBStoreSession({
 
 const app = express();
 const port = process.env.PORT || 3000;
-
 const sessionSecret = process.env.SESSION_SECRET;
+
 if (!sessionSecret) {
   throw new Error("SESSION_SECRET is not set");
 }
+
+app.use(morgan("dev"));
 
 app.use(
   session({
